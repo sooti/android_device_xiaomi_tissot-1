99a97c1f0dfc42bf912d62237b550802df6ea683
Bruno Martins <bgcngm@gmail.com>
2018-02-07 00:41:09 +0000

    tissot: Use MIUI poweroff charging animation
     * It's cool, and more importantly... it actually works!
       Oh look, it even displays battery level. Wow. :-P
    
    Change-Id: I6351e2eac6df89aa1b6b98e4e6b2815fa5c37794

--- device.mk
+++ device.mk
@@ -233,6 +233,7 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-convert \
     android.hardware.health@1.0-service \
     android.hardware.health@1.0
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
