0b4a6deb9bde05d3c125c94fec19d9e57c305172
sooti <sooti85@gmail.com>
2018-04-26 21:11:57 +0000

    tissot: back to lineage power

--- device.mk
+++ device.mk
@@ -313,9 +313,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.msm8953 \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-impl \
+    android.hardware.power@1.1-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
